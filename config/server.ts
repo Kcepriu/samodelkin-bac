f72e6cd39b43aabfcbfde58f311b01c5a81fa0c2
Kcepriu <kcepriu@gmail.com>
2024-01-20 16:19:14 +0200

    add language to message

--- config/server.ts
+++ config/server.ts
@@ -2,8 +2,8 @@ export default ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   // url: env("PUBLIC_URL", "http://localhost:1337"),
-  // url: env("PUBLIC_URL", "https://samodelkin-bac.kserhii.com"),
-  url: env("PUBLIC_URL", "https://samodelkin-bac.onrender.com"),
+  url: env("PUBLIC_URL", "https://samodelkin-bac.kserhii.com"),
+  // url: env("PUBLIC_URL", "https://samodelkin-bac.onrender.com"),
 
   url_front_end: env("URL_FRONT_END", ""),
   app: {
